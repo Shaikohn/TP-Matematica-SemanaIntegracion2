418790f436f8a19c9b75737e150646d8e0284d0e
Shai Kohn <shaicokohn@gmail.com>
2025-06-11 23:16:07 -0300

    Puntos 2A y 2B conectados

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import operacionesConDNI
+import operacionesConDNI, operacionesConAños
 
 def main():
-    operacionesConDNI.menu()
+    print("Bienvenido al programa de operaciones con DNI y años de nacimiento.")
+    print("Por favor, elija una de las siguientes opciones:")
+    print("1. Operaciones con DNI")
+    print("2. Operaciones con años de nacimiento")
+    print("3. Salir")
+    opcion = int(input("Selecciona una opción: "))
+    if opcion == 1:
+        operacionesConDNI.menu()
+    elif opcion == 2:
+        operacionesConAños.operacionesConAños()
+    elif opcion == 3:
+        print("Saliendo del programa.")
+    else:
+        print("Opción no válida. Por favor, intente de nuevo.")
 if __name__ == "__main__":
     main()
